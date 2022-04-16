1c505f0ff59c6d93d288759495e74aa4f181fbbf
Daniel Eddy <danieljeddy98@gmail.com>
2022-04-17 08:51:38 +1000

    Initial Commit

---
+++ Hubble/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Hubble
+//
+//  Created by Daniel Eddy on 17/4/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Hubble/HubbleApp.swift
@@ -0,0 +1,17 @@
+//
+//  HubbleApp.swift
+//  Hubble
+//
+//  Created by Daniel Eddy on 17/4/2022.
+//
+
+import SwiftUI
+
+@main
+struct HubbleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
